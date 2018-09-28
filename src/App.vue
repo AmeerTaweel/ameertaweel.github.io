8f77334e32042f559989f47bd91425be4ffc3990
Ameer Taweel <ameertaweel2002@gmail.com>
2018-09-28 18:31:54 +0300

    style: Add text animations and media queries

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
       <h1 class="hand-text position-absolute text-white bold-text p-4 w-100"><span class="text-dark">A</span>meer<span class="text-dark">T</span>aweel</h1>
       <div class="d-flex align-items-center justify-content-center h-100 w-100">
         <div class="position-fixed">
-          <h1 class="text-white font-weight-bold p-3">Android &amp; Full-Stack Developer</h1>
-          <h4 class="text-dark font-weight-bold text-uppercase">Passionate About Coding &amp; Making World Better</h4>
+          <h1 class="text-white animate-h1 p-3">Android &amp; Full-Stack Developer</h1>
+          <h4 class="text-dark animate-h4 text-uppercase">Passionate About Coding &amp; Making World Better</h4>
         </div>
       </div>
     </div>
     <div class="position-relative bg-white">
-      <h1 class="text-secondary p-5 font-weight-bold">My Projects</h1>
+      <h1 class="text-secondary p-5 animate-h1">My Projects</h1>
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6 col-lg-4" v-for="(project, index) in projects" :key="index">
@@ -20,32 +20,32 @@
       </div>
       <div class="container-fluid mt-5">
         <div class="row bg-light">
-          <div class="col-sm-12 col-md-6 col-lg-4 m-0 p-0">
+          <div class="col-sm-12 col-md-4 col-lg-4 m-0 p-0">
             <img class="w-100 h-auto" src="./assets/personal_pic.jpg" />
           </div>
-          <div class="col-sm-12 col-md-6 col-lg-8 p-5 text-left">
-            <h1 class="mb-3">Oh Hello!<br/>I'm Ameer Taweel</h1>
-            <h3 class="text-muted">I am an Android &amp; Full-Stack Developer based in Jerusalem,
+          <div class="col-sm-12 col-md-8 col-lg-8 px-5 py-3 text-left">
+            <h2 class="my-3 animate-h2">I'm Ameer Taweel</h2>
+            <h3 class="text-muted animate-p">I am an Android &amp; Full-Stack Developer based in Jerusalem,
               Palestine. I love <span class="text-main">coding</span>, <span class="text-main">
               gaming</span>, <span class="text-main">music</span>, <span class="text-main">
               reading</span>, and playing <span class="text-main">Football</span> with friends.</h3>
           </div>
         </div>
       </div>
-      <div class="container-fluid px-4 py-5 bg-dark text-left">
+      <div class="container-fluid p-5 bg-dark text-left">
         <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h1 class="font-weight-bold text-white">Get in touch</h1>
+          <div class="col-sm-12 col-md-6 my-3">
+            <h1 class="animate-h2 text-white">Get in touch</h1>
             <h3 class="text-secondary my-3">Got a project you would like me to work on? Or how about just a friendly chat?
-            Send me an email and we can grab a lovely coffee.</h3>
+            Send me an email on the adress below.</h3>
             <h3 class="font-weight-bold text-white">Email</h3>
-            <a href="mailto:ameeertaweel2002@gmail.com" target="_blank"><h3 class="text-main">ameeertaweel2002@gmail.com</h3></a>
+            <a class="link" href="mailto:ameeertaweel2002@gmail.com" target="_blank"><h3 class="text-main"><small>ameeertaweel2002@gmail.com</small></h3></a>
           </div>
-          <div class="col-sm-12 col-md-6">
-            <h1 class="font-weight-bold text-white">Networks</h1>
+          <div class="col-sm-12 col-md-6 my-3">
+            <h1 class="animate-h2 text-white">Networks</h1>
             <h3 class="text-secondary my-3">Give me a follow on these sites</h3>
-            <a href="https://www.linkedin.com/in/ameer-taweel" target="_blank"><h3 class="text-main">LinkedIn</h3></a>
-            <a href="https://github.com/AmeerTaweel" target="_blank"><h3 class="text-main">Github</h3></a>
+            <a class="link" href="https://www.linkedin.com/in/ameer-taweel" target="_blank"><h3 class="text-main">LinkedIn</h3></a>
+            <a class="link" href="https://github.com/AmeerTaweel" target="_blank"><h3 class="text-main">Github</h3></a>
           </div>
         </div>
         <div class="row">
@@ -93,12 +93,128 @@ export default {
   text-align: center;
 }
 
+.animate-h1{
+  font-size: 2.5em;
+  font-weight: bold;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -o-transition: all 0.3s;
+  transition: all 0.3s;
+}
+
+.animate-h2{
+  font-size: 2.5em;
+  font-weight: bold;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -o-transition: all 0.3s;
+  transition: all 0.3s;
+}
+
+.animate-h4{
+  font-size: 1em;
+  font-weight: bold;
+  -webkit-transition: all 0.3s 0.3s;
+  -moz-transition: all 0.3s 0.3s;
+  -o-transition: all 0.3s 0.3s;
+  transition: all 0.3s 0.3s;
+}
+
+.animate-p{
+  -webkit-transition: all 0.3s 0.3s;
+  -moz-transition: all 0.3s 0.3s;
+  -o-transition: all 0.3s 0.3s;
+  transition: all 0.3s 0.3s;
+}
+
+/*  Small Screens */
+@media only screen and (max-width: 767px) {
+  .animate-h1{
+    font-size: 2.5em;
+  }
+
+  .animate-h2{
+    font-size: 2em;
+    text-align: center;
+  }
+
+  .animate-h4{
+    font-size: 1em;
+  }
+
+  .animate-p{
+    font-size: 1em;
+    text-align: center;
+  }
+}
+
+/*  Medium Screens */
+@media only screen and (min-width:768px) and (max-width: 991px) {
+  .animate-h1{
+    font-size: 3em;
+  }
+
+  .animate-h2{
+    font-size: 2.5em;
+  }
+
+  .animate-h4{
+    font-size: 1.2em;
+  }
+
+  .animate-p{
+    font-size: 1.4em;
+  }
+}
+
+/*  Large Screens */
+@media only screen and (min-width:992px) and (max-width: 1199px) {
+  .animate-h1{
+    font-size: 4em;
+  }
+
+  .animate-h2{
+    font-size: 3em;
+  }
+
+  .animate-h4{
+    font-size: 1.6em;
+  }
+
+  .animate-p{
+    font-size: 2em;
+  }
+}
+
+/*  Extra Large Screens */
+@media only screen and (min-width:1200px) {
+  .animate-h1{
+    font-size: 4.5em;
+  }
+
+  .animate-h2{
+    font-size: 3.5em;
+  }
+
+  .animate-h4{
+    font-size: 1.8em;
+  }
+
+  .animate-p{
+    font-size: 2.5em;
+  }
+}
+
 .hand-text {
   font-family: 'Courgette', cursive;
 }
 
 .text-main {
   color: rgb(75, 184, 210);
+}
+
+.link:hover {
+  text-decoration-color: rgb(75, 184, 210);
 }
 
 .parallax {
