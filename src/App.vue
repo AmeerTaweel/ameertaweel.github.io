b38c55ffe589e290696ebaa2e14abaa957e88edd
Ameer Taweel <ameertaweel2002@gmail.com>
2019-06-12 21:14:36 +0300

    feat: Update intro

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app" class="h-100">
+    <!-- Name and Intro -->
     <div class="parallax-image-theme parallax">
       <div class="d-flex align-items-center justify-content-center h-100 w-100">
-        <div class="position-fixed">
-          <h1 class="text-white animate-h1 p-3">Ameer Mohannad Taweel</h1>
-          <h4 class="text-dark animate-h4 text-uppercase">Passionate About Coding &amp; Making The World Better</h4>
+        <div class="position-fixed text-white w-100 px-4">
+          <h1 class="c-h1 animate-text py-4">Ameer Mohannad Taweel</h1>
+          <h3 class="c-h3 animate-text">Passionate About Coding, Challenging Myself &amp; Learning New Stuff!</h3>
         </div>
       </div>
     </div>
     <div class="position-relative bg-white">
-      <h1 class="text-secondary p-5 animate-h1">My Projects</h1>
+      <h1 class="text-secondary p-5 c-h1 animate-text">My Projects</h1>
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6 col-lg-4" v-for="(project, index) in projects" :key="index">
@@ -23,8 +24,8 @@
             <img class="w-100 h-auto" src="../public/personal_pic.jpg" alt="Ameer Taweel's personal photo" />
           </div>
           <div class="col-sm-12 col-md-8 col-lg-8 px-5 py-3 text-left">
-            <h2 class="my-3 animate-h2">I'm Ameer Taweel</h2>
-            <h3 class="muted-text animate-p">I am an Android &amp; Full-Stack Developer based in Jerusalem,
+            <h2 class="my-3 c-h2 animate-text">I'm Ameer Taweel</h2>
+            <h3 class="muted-text c-p animate-text">I am an Android &amp; Full-Stack Developer based in Jerusalem,
               Palestine. I love <span class="text-main">coding</span>, <span class="text-main">
               gaming</span>, <span class="text-main">music</span>, <span class="text-main">
               reading</span>, and playing <span class="text-main">Football</span> with friends.</h3>
@@ -34,14 +35,14 @@
       <div id="footer" class="container-fluid p-4 bg-dark">
         <div class="row">
           <div class="col-sm-12 col-md-6 my-3">
-            <h1 class="animate-h2 text-white">Get in touch</h1>
+            <h1 class="c-h2 animate-text text-white">Get in touch</h1>
             <h3 class="text-light my-3">Got a project you would like me to work on? Or how about just a friendly chat?
             Send me an email on the adress below.</h3>
             <h3 class="font-weight-bold text-white">Email</h3>
             <a class="link" href="mailto:ameeertaweel2002@gmail.com" target="_blank"><h3 class="text-main"><small>ameeertaweel2002@gmail.com</small></h3></a>
           </div>
           <div class="col-sm-12 col-md-6 my-3">
-            <h1 class="animate-h2 text-white">Networks</h1>
+            <h1 class="c-h2 animate-text text-white">Networks</h1>
             <h3 class="text-light my-3">Give me a follow on these sites</h3>
             <a class="link" href="https://www.linkedin.com/in/ameer-taweel" rel="noopener noreferrer" target="_blank"><h3 class="text-main">LinkedIn</h3></a>
             <a class="link" href="https://github.com/AmeerTaweel" rel="noopener noreferrer" target="_blank"><h3 class="text-main">Github</h3></a>
@@ -87,39 +88,21 @@ export default {
 </script>
 
 <style lang="sass">
+$primary-color: #FFEB3B
+$secondary-color: #333
+
 #app
   font-family: 'Raleway', sans-serif
   text-align: center
 
-.animate-h1
-  font-size: 2.5em
-  font-weight: bold
+.animate-text
   -webkit-transition: all 0.3s
   -moz-transition: all 0.3s
   -o-transition: all 0.3s
   transition: all 0.3s
 
-.animate-h2
-  font-size: 2.5em
+.c-h1, .c-h2, .c-h3
   font-weight: bold
-  -webkit-transition: all 0.3s
-  -moz-transition: all 0.3s
-  -o-transition: all 0.3s
-  transition: all 0.3s
-
-.animate-h4
-  font-size: 1em
-  font-weight: bold
-  -webkit-transition: all 0.3s 0.3s
-  -moz-transition: all 0.3s 0.3s
-  -o-transition: all 0.3s 0.3s
-  transition: all 0.3s 0.3s
-
-.animate-p
-  -webkit-transition: all 0.3s 0.3s
-  -moz-transition: all 0.3s 0.3s
-  -o-transition: all 0.3s 0.3s
-  transition: all 0.3s 0.3s
 
 #footer
   text-align: left
@@ -129,60 +112,46 @@ export default {
   #footer
     text-align: center
 
-  .animate-h1
+  .c-h1
     font-size: 2.5em
 
-  .animate-h2
+  .c-h2
     font-size: 2em
-    text-align: center
 
-  .animate-h4
+  .c-h3, .c-p
     font-size: 1em
-
-  .animate-p
-    font-size: 1em
-    text-align: center
 
 /*  Medium Screens */
 @media only screen and (min-width:768px) and (max-width: 991px)
-  .animate-h1
+  .c-h1
     font-size: 3em
 
-  .animate-h2
+  .c-h2
     font-size: 2.5em
 
-  .animate-h4
-    font-size: 1.2em
-
-  .animate-p
-    font-size: 1.4em
+  .c-h3, .c-p
+    font-size: 1.5em
 
 /*  Large Screens */
 @media only screen and (min-width:992px) and (max-width: 1199px)
-  .animate-h1
+  .c-h1
     font-size: 4em
 
-  .animate-h2
+  .c-h2
     font-size: 3em
 
-  .animate-h4
-    font-size: 1.6em
-
-  .animate-p
+  .c-h3, .c-p
     font-size: 2em
 
 /*  Extra Large Screens */
 @media only screen and (min-width:1200px)
-  .animate-h1
+  .c-h1
     font-size: 4.5em
 
-  .animate-h2
+  .c-h2
     font-size: 3.5em
 
-  .animate-h4
-    font-size: 1.8em
-
-  .animate-p
+  .c-h3, .c-p
     font-size: 2.5em
 
 .text-main
@@ -195,7 +164,7 @@ export default {
   text-decoration-color: rgb(75, 184, 210)
 
 .parallax-image-theme
-  background: linear-gradient(rgba(75, 184, 210, 0.7), rgba(75, 184, 210, 0.7)), url('../public/background.png')
+  background: linear-gradient(to right bottom, rgba(89, 167, 255, 0.7), rgb(0, 31, 62)), url('../public/background.jpg')
 
 .parallax
   /* Full Height */
