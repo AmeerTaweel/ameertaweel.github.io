fe57d4e4618c8f274a07fdb9edb8816c678d636a
Ameer Taweel <ameertaweel2002@gmail.com>
2019-06-13 21:16:50 +0300

    feat: Setup website new sections

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,57 @@
 <template>
   <div id="app" class="h-100">
     <!-- Name and Intro -->
-    <div class="parallax-image-theme parallax">
+    <div class="parallax-image-intro parallax">
       <div class="d-flex align-items-center justify-content-center h-100 w-100">
-        <div class="position-fixed text-white w-100 px-4">
+        <div class="text-white w-100 px-4">
           <h1 class="c-h1 animate-text py-4">Ameer Mohannad Taweel</h1>
           <h3 class="c-h3 animate-text">Passionate About Coding, Challenging Myself &amp; Learning New Stuff!</h3>
+        </div>
+      </div>
+    </div>
+    <!-- About Me Section -->
+    <div class="position-relative bg-white">
+      <h1 class="text-secondary c-h1 m-5 animate-text">About Me</h1>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-6 col-sm-5 col-md-4 col-lg-3">
+            <img class="w-100 h-auto rounded-circle" src="../public/ameer-taweel.jpg" alt="Ameer Taweel's personal photo" />
+          </div>
+        </div>
+        <h3 class="muted-text c-p animate-text m-4 text-justify">
+          I am an Android and Full-Stack Developer based in Jerusalem, Palestine.
+          I'm passionate <span class="text-main">coding</span> and computer stuff&#128187;&#128293;.
+          I like <span class="text-main">learning</span> and <span class="text-main">challenging</span> myself&#128216;.
+          For entertainment I do <span class="text-main">gaming</span>&#127918;, listen to <span class="text-main">music</span>&#127911;,
+          play <span class="text-main">Chess</span>&#9823;, watch <span class="text-main">Anime</span>&#128122;, and play
+          <span class="text-main">Soccer</span> with friends&#9917;.
+        </h3>
+      </div>
+    </div>
+    <!-- Skills Intro -->
+    <div class="parallax-image-skills parallax">
+      <div class="d-flex align-items-center justify-content-center h-100 w-100">
+        <div class="text-white w-100 px-4">
+          <h1 class="c-h1 animate-text py-4">Skills and Abilities</h1>
+          <h3 class="c-h3 animate-text">“Knowledge is power.”<br/>- Francis Bacon</h3>
+        </div>
+      </div>
+    </div>
+    <!-- Projects Intro -->
+    <div class="parallax-image-projects parallax">
+      <div class="d-flex align-items-center justify-content-center h-100 w-100">
+        <div class="text-white w-100 px-4">
+          <h1 class="c-h1 animate-text py-4">Projects</h1>
+          <h3 class="c-h3 animate-text">“Those at the top of the mountain didn’t fall there.”<br/>- Marcus Washling</h3>
+        </div>
+      </div>
+    </div>
+    <!-- Experience Intro -->
+    <div class="parallax-image-experiences parallax">
+      <div class="d-flex align-items-center justify-content-center h-100 w-100">
+        <div class="text-white w-100 px-4">
+          <h1 class="c-h1 animate-text py-4">Experiences</h1>
+          <h3 class="c-h3 animate-text">“Experience is the mother of wisdom.”<br/>- Unknown</h3>
         </div>
       </div>
     </div>
@@ -118,8 +164,11 @@ $secondary-color: #333
   .c-h2
     font-size: 2em
 
-  .c-h3, .c-p
+  .c-h3
     font-size: 1em
+
+  .c-p
+    font-size: 2em
 
 /*  Medium Screens */
 @media only screen and (min-width:768px) and (max-width: 991px)
@@ -163,8 +212,17 @@ $secondary-color: #333
 .link:hover
   text-decoration-color: rgb(75, 184, 210)
 
-.parallax-image-theme
-  background: linear-gradient(to right bottom, rgba(89, 167, 255, 0.7), rgb(0, 31, 62)), url('../public/background.jpg')
+.parallax-image-intro
+  background: linear-gradient(to right bottom, rgba(89, 167, 255, 0.7), rgb(0, 31, 62)), url('../public/bg-intro.jpg')
+
+.parallax-image-projects
+  background: linear-gradient(to right bottom, rgba(89, 167, 255, 0.7), rgb(0, 31, 62)), url('../public/bg-projects.jpg')
+
+.parallax-image-skills
+  background: linear-gradient(to right bottom, rgba(89, 167, 255, 0.7), rgb(0, 31, 62)), url('../public/bg-skills.jpg')
+
+.parallax-image-experiences
+  background: linear-gradient(to right bottom, rgba(89, 167, 255, 0.5), rgba(0, 31, 62, 0.7)), url('../public/bg-experiences.jpg')
 
 .parallax
   /* Full Height */
