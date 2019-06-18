0397a3d01608a27f34dd381c144c64865b0858a5
Ameer Taweel <ameertaweel2002@gmail.com>
2019-06-18 22:50:32 +0300

    feat: Fix font loading problem

--- src/App.vue
+++ src/App.vue
@@ -344,9 +344,12 @@ $primary-color: #FFEB3B
 $secondary-color: #333
 
 @font-face
-  font-family: Raleway
+  font-family: 'Raleway'
   font-display: swap
-  src: url(https://fonts.googleapis.com/css?family=Raleway)
+  font-style: normal
+  font-weight: 400
+  src: local('Raleway'), local('Raleway-Regular'), url(https://fonts.gstatic.com/s/raleway/v13/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2')
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD
 
 #app
   font-family: 'Raleway', sans-serif
