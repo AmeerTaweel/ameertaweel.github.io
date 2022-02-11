6efc3e59828257df4de4ebcf7cc1fddd298503c3
Ameer Taweel <ameertaweel2002@gmail.com>
2022-02-11 21:16:36 +0300

    feat: Initialize new Hugo website

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+	description = "Personal Website";
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+	inputs.flake-utils.url = "github:numtide/flake-utils";
+
+	outputs = { self, nixpkgs, flake-utils }:
+	flake-utils.lib.eachDefaultSystem (system: let
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		devShell = pkgs.mkShell {
+			nativeBuildInputs = with pkgs; [
+				hugo
+			];
+			buildInputs = [ ];
+		};
+	});
+}
